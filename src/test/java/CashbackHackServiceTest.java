fe2b966e02ea0ff84a93867d5072f71896e33698
Антон Журавлев <admin@Mac-mini-Anton.local>
2025-10-17 23:36:30 +0800

    first commit

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,44 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.netology.service.CashbackHackService;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldRemainderIfTheAmountIsLessThenTheSum() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int result = service.remain(amount);
+
+        Assert.assertEquals(result, 100);
+    }
+
+    @Test
+    public void shouldReturnIfTheAmountIsAboveTheMaximum() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1300;
+
+        int result = service.remain(amount);
+
+        Assert.assertEquals(result, 700);
+    }
+
+    @Test
+    public void shouldReturnIfTheAmountIsMinimal() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+
+        int result = service.remain(amount);
+
+        Assert.assertEquals(result, 999);
+    }
+
+
+
+
+
+
+
+
+}
